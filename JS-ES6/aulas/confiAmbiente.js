4c53fbca0bb7cbf3f4ad5eed582e159143746efb
Talita Azevedo <talita.azevedo360@gmail.com>
2019-09-28 12:42:02 -0300

    aprendendo sobre ES6 configurando o webpack server

--- JS-ES6/aulas/confiAmbiente.js
+++ JS-ES6/aulas/confiAmbiente.js
@@ -2,5 +2,97 @@
 # Yarn é um gerenciador de pacotes do Javascript
 ###Permite a instalação de apis de outras pessoas no ambiente
 ### Node é um compilador
+#### Comanddo do yarn ::::yarn init => inicializa o yarn na pasta raiz do preojeto
+### yarn add @babel/cli para instalar o babel no meu ambiente
+### yarn add @babel / preset - env  => instalar esta dependencia também
+## ignorar a pasta node_modules
+## criar um arquivo .babelrc
+## no arquivo package.json 
+### criar a linha abaixo
+"scripts": {
+    ### linha dev => executa o babel no arquivo main.js e -o envia para um arquivo bundle.js compilado
+    "dev": "babel ./main.js -o ./bundle.js  -w"
+    
+
+}
+
+### yarn add @babel / core mais um pacote para adicionar;
+##
+executar o comando yarn dev ele vai criar o arquivo bundle.js
+##Comando para instalar outro pacote do babel sobre rest e spread do ecma script
+yarn add @babel / plugin - proposal - object - rest - spread
+## configurando o webpack
+## > yarn add webpack webpack - cli
+// as dependencias so são necessárias no ambiente de desenvolvimento
+o que vai para produção é o arquivo bbundle.js
+para definir o que vai para o ambiente de produção 
+criar um arquivo webpack.config.js
+##
+adicionar o modulo yarn add babel-loader
+
+#### configuração do webpack
+module.exports = {
+    // arquivo de entrada
+    entry: './main.js',
+    output: {
+        //isso aqui é uma var global obrigatoria
+        path: __dirname,
+        // saida neste arquivo que é o que vai para produção
+        filename: './bundle.js',
+    },
+    module: {
+        rules: [{
+            // procura todos os arquivos com a extensão js
+                test: /\.js$/,
+                // exclui a pasta node modules
+                exclude: /node_modules/,
+                // carrega o modulo do babel
+                use: {
+                    loader: 'babel-loader',
+                }
+
+            }
+
+        ],
+    }
+
+};
+
+### Para testar o webpack 
+
+### vá até o arquivo packaje.json 
+altere a propriedade para o seguinte
+
+ "scripts": {
+     "dev": "webpack --mode-development -w"
+ },
+
+ ### Instalando o webpack dev server 
+yarn add webpack - dev - server - D
+Mudar os arquivos de configuração no webpack.config
+
+fica assim 
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        // mudou o path
+        path: __dirname + '/public',
+        filename: './bundle.js',
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+
+            }
+
+        ],
+    }
+
+};
+
 
 */
---
+++ JS-ES6/src/main.js
@@ -0,0 +1,204 @@
+/*const arr = [1, 3, 4, 5, 8, 9];
+const newArr = arr.map(function (item, index) {
+
+    return item + index;
+});
+
+console.log(newArr);
+
+
+const sum = arr.reduce(function (total, next) {
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function (item) {
+    return item % 2 === 0;
+
+});
+
+console.log(filter);
+
+const find = arr.find(function (item) {
+    return item === 4;
+});
+
+console.log(find);
+*/
+
+// arrow functions
+/*
+const arr = [1, 3, 4, 5, 6];
+const newArr = arr.map(item => item * 2);
+console.log(newArr);
+
+const teste = () => ({
+    nome: 'Talita'
+});
+console.log(teste());
+*/
+/*
+function soma(a = 0, b = 3) {
+    return a + b;
+};
+console.log(soma(1));
+*/
+/*
+const soma = (a = 0, b = 3) => a + b;
+console.log(soma(1));
+*/
+
+
+
+
+/**
+ * 
+
+// desestruturação
+
+
+
+
+
+const usuario = {
+    nome: 'Talita',
+    idade: 23,
+    endereco: {
+        cidade: 'Osasco',
+        Estado: 'SP',
+    },
+
+}
+console.log(usuario);
+// isso aqui é uma ddesestruturação
+const {
+    nome,
+    idade,
+    endereco: {
+        cidade
+    }
+} = usuario;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+
+// outro exemplo
+
+function mostraNome({
+    nome,
+    idade
+}) {
+    console.log(nome, idade);
+}
+
+console.log(mostraNome(usuario));
+ */
+
+// operadores rest e spread
+// antes de utilizar  fazer o seguinte comando yarn add @babel/plugin-proposal-object-rest-spread
+
+
+
+// Primeiro falar sobre REST
+// serve para pegar o resto das propriedades
+
+/**
+ *
+ const usuario = {
+     nome: 'Talita',
+     idade: 28,
+     empresa: 'Rocketseat'
+ };
+ const {
+     nome,
+     ...resto
+ } = usuario;
+
+ console.log(nome);
+ console.log(resto);
+
+ const arr = [1, 2, 3, 4, 5];
+
+ const [a, b, ...c] = arr;
+ console.log(a);
+ console.log(b);
+ console.log(c);
+
+ function soma(a, b, ...params) {
+     return params;
+
+ }
+
+ console.log(soma(1, 3));
+
+
+ // conceito de spread
+
+ const arr1 = [1, 2, 3];
+ const arr2 = [4, 5, 6];
+ const arr3 = [...arr1, ...arr2];
+ console.log(arr3);
+
+
+ const usuario1 = {
+     nome: 'Talita',
+     idade: 28,
+     empresa: 'Rocketseat'
+
+ };
+ const usuario2 = {
+     ...usuario1,
+     nome: 'Diego'
+ };
+ console.log(usuario2);
+ */
+
+// aula sobre template literals 
+/*
+const nome = 'Talita';
+const idade = 28;
+console.log('Meu nome é ' + nome + ' e tenho ' + idade);
+// agora com o template literals
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
+
+
+* */
+
+
+// aula sobre objecy short syntax
+
+/**
+const nome = 'Talita';
+const idade = 28;
+
+
+const usuario = {
+    // no ES6 não precisa declarar nome:nome, se o nome da variavel for igual ao nome do objeto
+    nome,
+    idade,
+    empresa: 'Rocketseat',
+}
+
+console.log(usuario);
+
+
+*/
+
+// configurando o webpack
+// após a configuração do webpak é possível importar arquivos.
+/*import {
+    soma
+} from './funcoes';
+console.log(soma(1, 2));
+*/
+// quando define o export default pode também importar da forma como abaixo
+
+import somaFuncction from './funcoes';
+import * as funcoes from './funcoes';
+console.log(funcoes);
+console.log(funcoes.soma(1, 2));
+console.log(funcoes.sub(3, 4));
+console.log(funcoes.multi(5, 7));
