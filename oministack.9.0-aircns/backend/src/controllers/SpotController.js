78d12b1cab124b2e486d05be63be06f91fa30d0c
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-01 23:53:52 -0300

    criando um spot => uploads já estão funcionando

--- oministack.9.0-aircns/backend/src/controllers/SpotController.js
+++ oministack.9.0-aircns/backend/src/controllers/SpotController.js
@@ -1,7 +1,29 @@
+const Spot = require('../models/Spot');
 module.exports = {
     async store(req, res) {
-        return res.json({
-            ok: true
-        });
+
+        const {
+            filename
+        } = req.file;
+
+        const {
+            company,
+            techs,
+            price
+        } = req.body;
+
+        const {
+            user_id,
+        } = req.headers;
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price,
+
+        })
+        return res.json(spot);
     }
 }
