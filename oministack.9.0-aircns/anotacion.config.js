d93501beb9d37fee2a662d83240447a51c6fb5eb
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-01 22:42:42 -0300

    Arquivos de configuração alterados

--- oministack.9.0-aircns/anotacion.config.js
+++ oministack.9.0-aircns/anotacion.config.js
@@ -3,6 +3,16 @@ dependencias
 
 yarn add express
 yarn add nodemon - D
+insominia.rest testar APIs
+yarn add mongoose
+
+
+package.json
+"scripts": {
+    "dev": "nodemon src/server.js"
+
+},
+
 
 
 
