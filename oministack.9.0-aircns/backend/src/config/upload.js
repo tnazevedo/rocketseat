3b151928c24015a9fb2cbd4e8f1431df5bce41cd
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-01 23:56:02 -0300

    Separando comandos em variaveis

--- oministack.9.0-aircns/backend/src/config/upload.js
+++ oministack.9.0-aircns/backend/src/config/upload.js
@@ -7,7 +7,9 @@ module.exports = {
         //legal utilizando este comando ele não precisa ../
         destination: path.resolve(__dirname, '..', '..', 'uploads'),
         filename: (req, file, cb) => {
-            cb(null, `¨${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+            const ext = path.extname(file.originalname);
+            const name = path.basename(file.originalname, ext);
+            cb(null, `${name}-${Date.now()}${ext}`);
         }
     }),
 };
