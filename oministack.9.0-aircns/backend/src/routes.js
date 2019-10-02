10335a84edd0ef6057d9c42f2b72447f442f07a2
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-02 00:51:18 -0300

    adicionando route do booking

--- oministack.9.0-aircns/backend/src/routes.js
+++ oministack.9.0-aircns/backend/src/routes.js
@@ -15,18 +15,21 @@ const routes = express.Router();
 const upload = multer(uploadConfig);
 
 
-
+// Importando controladores 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 
-// Routes
+// Criando Rotas 
 routes.post('/sessions', SessionController.store);
 
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 routes.get('/dashboard', DashboardController.show);
+//essa rotafaz uma coisa legal ela é bem descritiva pega o id
+routes.post('spots/:id/booking', BookingController.store);
 
 
 module.exports = routes;
