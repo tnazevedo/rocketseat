5ed49ad3460c109734e49d0f4a70ddcc8b46195c
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-01 22:39:41 -0300

    adicionando conexão assincrona para pesquisa e criação de e-mail durante as sessions

--- oministack.9.0-aircns/backend/src/controllers/SessionController.js
+++ oministack.9.0-aircns/backend/src/controllers/SessionController.js
@@ -1,3 +1,23 @@
+// index  , show, store, destroy
+const User = require('../models/User');
 module.exports = {
+    async store(req, res) {
+        const {
+            email
+        } = req.body;
+        // Primeiro a var recebe o metodo User que procura  um e-mail
+        let user = await User.findOne({
+            email
+        });
+        // se ele não achar o usuário ele vai criar, caso ele encontre não criará retornará com o mesmo ID,
+        if (!user) {
+            user = await User.create({
+                email
+            });
+        }
+
+        return res.json(user);
+    }
+
 
 };
