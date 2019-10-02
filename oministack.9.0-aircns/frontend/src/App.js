44fc145c784043b7518efda4257fbe491bd4ad2e
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-02 15:39:44 -0300

    configurando function handleSubmit

--- oministack.9.0-aircns/frontend/src/App.js
+++ oministack.9.0-aircns/frontend/src/App.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "./services/api";
 import "./App.css";
 import logo from "./assets/logo.svg";
 
 function App() {
+
+
+
+  const [email, setEmail] = useState("");
+  
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Funcionou mano!! Tá porra" + email);
+    const response = await api.post('/sessions', {email});
+    console.log(response);
+
+
+  }
   return (
     <div className="container">
       <img src={logo} alt="AirCnc" />
       <div className="content">
         <p>
-          
           Ofereça <strong> spots </strong> para os programadores e encontre
           <strong> talentos </strong> para sua empresa
         </p>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email"> E - MAIL * </label>
-          <input type="email" id="email" placeholder="Seu Melhor E-mail" />
-          <button className="btn"type="submit"> Entrar </button>
+          <input
+            type="email"
+            id="email"
+            placeholder="Seu Melhor E-mail"
+            value ={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+          <button className="btn" type="submit">
+            {" "}
+            Entrar{" "}
+          </button>
         </form>
       </div>
     </div>
