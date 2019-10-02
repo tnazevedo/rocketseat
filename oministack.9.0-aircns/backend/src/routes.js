8c4a8a029580bc2bb162abd271285e92289d9a0f
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-01 22:56:44 -0300

    Adicionando Route do model Spot

--- oministack.9.0-aircns/backend/src/routes.js
+++ oministack.9.0-aircns/backend/src/routes.js
@@ -8,8 +8,13 @@
 const express = require('express');
 const routes = express.Router();
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
+
+// Routes
 routes.post('/sessions', SessionController.store);
+
+routes.post('/spots', SpotController.store);
 
 
 module.exports = routes;
