514f32ab9e6c93cf75564abdf283d6b5628b2063
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-02 15:37:53 -0300

    Adicionanddo cors no backend

--- oministack.9.0-aircns/backend/src/server.js
+++ oministack.9.0-aircns/backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 mongoose.connect('mongodb+srv://air-cnc:aircnc1@aircnc-ztiqe.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -8,6 +9,6 @@ mongoose.connect('mongodb+srv://air-cnc:aircnc1@aircnc-ztiqe.mongodb.net/semana0
 const routes = require('./routes');
 //porta que a aplicação sera iniciada no servidor
 app.listen(3333);
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
